601f921183da25a896f5cd056bd9ee1a80e297bc
clauddr <cla_udiu2006@yahoo.com>
2023-06-20 10:01:07 +0100

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
+export const dynamic = "force/dynamic";
+
 interface HomeProps {
   searchParams: IListingsParams;
 }
-
-export const dynamic = "force/dynamic";
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
